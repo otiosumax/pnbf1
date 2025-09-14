9fe689362fffb73444492620fc472c664c81f1c1
PRPDSS <prpdss@gmail.com>
2025-09-14 13:36:04 +0300

    initial commit

---
+++ frontend/src/components/MyComponent.vue
@@ -0,0 +1,7 @@
+<template> 
+    <div class="my-component"> 
+        <h2>Мой компонент</h2>
+        <p>Первый вью компонент, немного нервничаю</p>
+        <button href="t.me/otiosumax">Тык</button>
+    </div>
+</template>
